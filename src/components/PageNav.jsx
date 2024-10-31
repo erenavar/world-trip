cb546b9ef917152a6d2cb0fdad3112a5dcbd6156
Eren AVAR <eren.avar@hotmail.com>
2024-10-31 05:12:53 +0300

    create css module and add some css values

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./PageNav.modules.css";
 
 export default function PageNav() {
   return (
-    <nav>
+    <nav className="nav">
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import PageNav from "../components/PageNav";
 
 export default function HomePage() {
   return (
     <div>
-      <div>HomePage</div>;
+      <div>HomePage</div>
     </div>
   );
 }
