36b37f1b070d1c9d9413b8bf9371993fb2242a43
Eren AVAR <eren.avar@hotmail.com>
2024-11-04 03:50:15 +0300

    complete pages and its css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import PricingPage from "./pages/PricingPage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/LoginPage";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="product" element={<ProductPage />} />
           <Route path="pricing" element={<PricingPage />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+// Uses the same styles as Product
 import PageNav from "../components/PageNav";
+import styles from "./Product.module.css";
 
-export default function PricingPage() {
+export default function Pricing() {
   return (
-    <div>
+    <main className={styles.product}>
       <PageNav />
-    </div>
+      <section>
+        <div className={styles.row}>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img
+          src="../../public/img-2.jpg"
+          alt="overview of a large city with skyscrapers"
+        />
+      </section>
+    </main>
   );
 }
