bd28725b8932717bec32931801010e81dc203696
Eren AVAR <eren.avar@hotmail.com>
2024-11-09 22:39:15 +0300

    assign index pages

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,15 @@ export default function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route index element={<HomePage />} />
           <Route path="product" element={<ProductPage />} />
           <Route path="pricing" element={<PricingPage />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} >
-            <Route path="cities" element={<p>Cities</p>}/>
-            <Route path="countries" element={<p>Countries</p>}/>
-            <Route path="form" element={<p>Form</p>}/>
+            <Route index element={<p>List will be here...</p>} />
+            <Route path="cities" element={<p>Cities</p>} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
