37da52786dced36b49f051a0891ef473769ac575
Eren AVAR <eren.avar@hotmail.com>
2024-11-16 22:23:12 +0300

    change date format with date formatter function

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import styles from "./CityItem.module.css";
 
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
 function CityItem({ city }) {
   const { cityName, emoji, date } = city;
+
   return (
     <li className={styles.cityItem}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
-      <h3 className={styles.date}>{date}</h3>
+      <h3 className={styles.date}>{formatDate(date)}</h3>
     </li>
   );
 }
