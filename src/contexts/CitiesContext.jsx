adfd84cf07acdb6048bda3b3c9947960df3c7ed1
Eren AVAR <eren.avar@hotmail.com>
2024-12-15 18:42:02 +0300

    create use cities function

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CitiesContext = createContext();
 const BASE_URL = "http://localhost:9000";
@@ -35,4 +35,12 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("Cities context was used outside the CitiesContext");
+
+  return context;
+}
+
+export { CitiesProvider, useCities };
